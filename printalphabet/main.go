f76ad96df5b8d2c603815b2e9fb2c6955818025d
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-05 21:43:42 +0200

    test

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,12 +1,11 @@
 package main
 
-import (
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 func main() {
 	for i := 97; i <= 122; i++ {
-		z01.PrintRune(rune(i))
+		c := rune(i)
+		z01.PrintRune(c)
 	}
 	z01.PrintRune('\n')
 }
