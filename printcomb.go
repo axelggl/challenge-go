2523200781e6983b4c96973e67367bce4841cd34
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-06 21:31:35 +0200

    euh les nombres dans l'ordre

---
+++ printcomb.go
@@ -0,0 +1,43 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func main() {
+	for i := 48; i <= 57; i++ {
+		a := rune(i)
+
+		for j := 48; j <= 57; j++ {
+			b := rune(j)
+
+			for k := 48; k <= 57; k++ {
+				c := rune(k)
+
+				if i < j && j < k {
+					z01.PrintRune(a)
+					z01.PrintRune(b)
+					z01.PrintRune(c)
+					z01.PrintRune(44)
+					z01.PrintRune(32)
+				}
+			}
+		}
+	}
+}
+
+/*
+   i = 0
+   j = 0
+   k = 0
+
+   for i = 0; i <=9; i++
+   	for j = 0; j <=9; j++
+   		for k = 0; k >=9; k++
+   			if i > j && j > k
+   			print i j k
+
+
+
+   || = ou ; && = et
+
+
+*/
