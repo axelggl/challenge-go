b7dbe2fdf29692c04fd15f0aeef8bcd47860184a
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-07 15:20:18 +0200

    firstrune

--- firstrune.go
+++ firstrune.go
@@ -1,7 +1,6 @@
 package piscine
 
 func FirstRune(s string) rune {
-
-	s_runes := []rune(s) // crée un tableau avec s au format rune (ASCII)
+	s_runes := []rune(s) // crée un tableau avec s au format rune en ASCII
 	return s_runes[0]
 }
