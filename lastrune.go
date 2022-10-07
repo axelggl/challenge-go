7b035a0a06875da50ad0402ddd104a27347d6747
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-07 16:30:41 +0200

    lastrune

---
+++ lastrune.go
@@ -0,0 +1,12 @@
+package piscine
+
+func LastRune(s string) rune {
+	s_runes := []rune(s)
+	index := len(s_runes)
+	return s_runes[index-1]
+}
+
+// [] permet de créer un tableau dénombré de 0 à n où 0 est le premier charactère
+// len permet de donner la longueur d'un string de 1 à n, ex: "Salut c'est moi" va donner une len de 15 où S est 1, a est 2, etc.
+// on met index-1 car si on avait mis index tout court on aurait eu une erreur dans le output 
+// exemple d'erreur : panic: runtime error: index out of range [6] with length 6
