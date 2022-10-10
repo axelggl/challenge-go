bc449c89c17babd332d66f0de3f26fa56ef55438
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-10 11:46:41 +0200

    printstr

---
+++ printstr.go
@@ -0,0 +1,11 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintStr (s string) {
+	s_runes := []rune(s)
+	index := len(s_runes)
+	for i := 0; i < (index-1); i++ {
+		z01.PrintRune(s_runes[i])
+	}
+}
