5bdc57307c1fe71ec9f314aab2ede09b93f34461
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-10 11:47:53 +0200

    printstr

--- printstr.go
+++ printstr.go
@@ -5,7 +5,7 @@ import "github.com/01-edu/z01"
 func PrintStr(s string) {
 	s_runes := []rune(s)
 	index := len(s_runes)
-	for i := 0; i < (index-1); i++ {
+	for i := 0; i < (index - 1); i++ {
 		z01.PrintRune(s_runes[i])
 	}
 }
