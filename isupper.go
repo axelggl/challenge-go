93e6d5946947f5af146565356c9899bd1cca3957
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-11 16:27:18 +0200

    isupper

--- isupper.go
+++ isupper.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsUpper(s string) bool {
     my_array := []rune(s) // on crée un tableau de rune s
-    a := true             // a est affecté a true car si on return simplement true il quitte la condition et return simplement au fichier main
+    a := true // a est affecté a true car si on return simplement true il quitte la condition et return simplement au fichier main
     for _, r := range my_array {
         if !(r >= 65 && r <= 90) { // si la case [i] qui s'imcrémente a chaque fois // i = 0 tant que i est < à la longeur du tableau, alors i s'incrémente
             a = false // donc la variable a est affecté a vrai
