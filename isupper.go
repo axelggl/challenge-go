2cd1c93628a0911378b5fa8511acca5c9f4c557c
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-11 15:59:09 +0200

    isupper

--- isupper.go
+++ isupper.go
@@ -1,16 +1,14 @@
 package piscine
 
 func IsUpper(s string) bool {
-	my_array := []rune(s)
+	t := []rune(s)
 	a := false
-
-	for i := 0; i <= len(my_array)-1; i++ {
-		if my_array[i] >= 65 && my_array[i] <= 90 {
+	for _, r := range t {
+		if r >= 65 && r <= 90 {
 			a = true
 		} else {
 			a = false
 		}
 	}
-
 	return a
 }
