93d9743237af99a9e08160f8faa3876d17473f77
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-11 16:02:35 +0200

    isupper

--- isupper.go
+++ isupper.go
@@ -1,14 +1,14 @@
 package piscine
 
 func IsUpper(s string) bool {
-	t := []rune(s)
-	a := false
-	for _, r := range t {
-		if r >= 65 && r <= 90 {
-			a = true
-		} else {
-			a = false
-		}
-	}
-	return a
+    runeBoard := []rune(s)
+    a := true
+    for _, r := range runeBoard {
+        if r >= 65 && r <= 90 {
+            a = true
+        } else {
+            a = false
+        }
+    }
+    return a
 }
