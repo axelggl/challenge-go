b798cc474d9cc62de4cef6947348469a44caca63
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-11 17:13:39 +0200

    alphacount

--- alphacount.go
+++ alphacount.go
@@ -4,7 +4,7 @@ func AlphaCount(s string) int {
 	my_array := []rune(s)
 	k := 0
 	for _, r := range my_array {
-		if r >= 65 && r <= 90 && r >= 97 && r <= 122 {
+		if r >= 65 && r <= 90 || r >= 97 && r <= 122 {
 			k++
 		}
 	}
