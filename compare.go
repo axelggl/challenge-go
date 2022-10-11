38bbeb4ca5d256de4534fa3dbf48e7f5f8bcc37a
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-11 10:31:06 +0200

    compare

--- compare.go
+++ compare.go
@@ -1,12 +1,12 @@
 package piscine
 
-func Compare(a, b string) int {	
-		if a == b {
-			return 0
-		} else if a < b {
-			return -1
-		} else if a > b {
-			return 1
-		}
-		return(0)
+func Compare(a, b string) int {
+	if a == b {
+		return 0
+	} else if a < b {
+		return -1
+	} else if a > b {
+		return 1
+	}
+	return (0)
 }
