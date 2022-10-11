d1277971f1c90f7a05f881d17695ceac456eb898
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-11 16:32:39 +0200

    isupper

--- isupper.go
+++ isupper.go
@@ -4,9 +4,9 @@ func IsUpper(s string) bool {
 	my_array := []rune(s)
 	a := true
 	for _, r := range my_array {
-        if !(r >= 65 && r <= 90) {
-            a = false
-        }
-    }
-    return a
+		if !(r >= 65 && r <= 90) {
+			a = false
+			}
+		}
+	return a
 }
