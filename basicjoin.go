d2db91ec0bf905187818a1b9b77a43fc0dc07752
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-12 21:52:16 +0200

    basicjoin

--- basicjoin.go
+++ basicjoin.go
@@ -2,7 +2,7 @@ package piscine
 
 func BasicJoin(elems []string) string {
 	var final_string string // revient à faire final_string := ""
-	for _, s := range elems {
+	for _, s := range elems { // taille tableau
 		final_string += s
 	}
 	return final_string
