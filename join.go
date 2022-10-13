15c912694851a26b37f2e13f0ac2aea72088dad2
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-13 10:08:03 +0200

    join

--- join.go
+++ join.go
@@ -4,7 +4,7 @@ func Join(strs []string, sep string) string {
 	var final_string string
 	for i, s := range strs {
 		final_string += s
-		if i != len(strs) - 1 {
+		if i != len(strs)-1 {
 			final_string += sep
 		}
 	}
