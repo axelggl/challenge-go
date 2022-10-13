a36da8c6156a82e7237e5c41564c59bc065a76fb
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-13 10:07:39 +0200

    join

--- join.go
+++ join.go
@@ -2,9 +2,11 @@ package piscine
 
 func Join(strs []string, sep string) string {
 	var final_string string
-	for _, s := range strs {
+	for i, s := range strs {
 		final_string += s
-		final_string += sep
+		if i != len(strs) - 1 {
+			final_string += sep
+		}
 	}
 	return final_string
 }
