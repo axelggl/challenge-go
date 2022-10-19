07f167c0efad720f09531a987b1bc05fa7f5b08a
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-19 20:54:43 +0200

    index

--- index.go
+++ index.go
@@ -1,27 +1,23 @@
 package piscine
 
 func Index(s string, toFind string) int {
-    my_array := []rune(s)
-    find_letter := []rune(toFind)
+    sTable := []rune(s)
+    fTable := []rune(toFind)
 
-    var final int
-    for i, r := range my_array { // on vient rcupérer notre tableau my_array
-        if find_letter[0] == r { // si la première est = r
-            for _, k := range find_letter { // regarde le contenue et
-                if k == r {
+    if len(s) < 1 {
+        return 0
+    } else if len(s) < len(toFind) {
+        return -1
+    }
+
+    for i := 0; i < len(sTable); i++ {
+        if sTable[i] == fTable[0] {
+            for j := 0; j < len(toFind); j++ {
+                if fTable[j] == sTable[i+1] {
                     return i
-                }
-            }
-        } else if find_letter[0] != r {
-            for l, k := range find_letter {
-                if k == r {
-                    a := l
-
-                    final := a
-                    return -final
                 }
             }
         }
     }
-    return final
+    return -1
 }
