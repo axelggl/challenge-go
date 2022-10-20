23383cab4cac787a63b02f9fa0fc4171752601b2
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-20 11:02:55 +0200

    index

--- index.go
+++ index.go
@@ -1,24 +1,24 @@
 package piscine
 
 func Index(s string, toFind string) int {
-    sTable := []rune(s)
-    fTable := []rune(toFind)
+	sTable := []rune(s)
+	fTable := []rune(toFind)
 
-    if len(s) < 1 {
-        return 0
-    } else if len(s) < len(toFind) {
-        return -1
-    }
-    for i := 0; i < len(sTable); i++ {
-        if sTable[i] == fTable[0] {
-            for j := 0; j < len(toFind); j++ {
-                if sTable[i+1] == fTable[j] {
-                    return i
-                } else if sTable[i+1] != fTable[j] {
-                    return i
-                }
-            }
-        }
-    }
-    return -1
+	if len(s) < 1 {
+		return 0
+	} else if len(s) < len(toFind) {
+		return -1
+	}
+	for i := 0; i < len(sTable); i++ {
+		if sTable[i] == fTable[0] {
+			for j := 0; j < len(toFind); j++ {
+				if sTable[i+1] == fTable[j] {
+					return i
+				} else if sTable[i+1] != fTable[j] {
+					return i
+				}
+			}
+		}
+	}
+	return -1
 }
