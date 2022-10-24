c08ff6e1b4a41a003a8f16d56deb980494c9fdbf
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-24 14:49:28 +0200

    index

--- index.go
+++ index.go
@@ -19,31 +19,3 @@ func Index(s string, toFind string) int {
 	}
 	return index
 }
-
-
-
-
-
-/*func Index(s string, toFind string) int {
-	sTable := []rune(s)
-	fTable := []rune(toFind)
-
-	if len(s) < 1 {
-		return 0
-	} else if len(s) < len(toFind) {
-		return -1
-	}
-	for i := 0; i < len(sTable); i++ {
-		if sTable[i] == fTable[0] {
-			for j := 0; j < len(toFind); j++ {
-				if sTable[i+j] == fTable[j] {
-					return i
-				} else if sTable[i+j] != fTable[j] {
-					return i
-				}
-			}
-		}
-	}
-	return -1
-}
-*/
