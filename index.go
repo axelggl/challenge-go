2591e5e13ac3679f7698e652841ef0bd0c210d2e
MAERTEN Axel <axel.maerten@ynov.com>
2022-10-26 09:08:27 +0200

    index

--- index.go
+++ index.go
@@ -1,21 +1,31 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	var index int
-	for i := 0; i < len(s); i++ {
-		if s[i] == toFind[0] {
-			index = i
-			if len(toFind) == 1 {
-				return index
-			}
-			for j := 1; j < len(toFind) && j+i < len(s); j++ {
-				if s[i] != toFind[j] {
-					return -1
-				} else if s[i] == toFind[len(toFind)-1] {
-					return index
-				}
-			}
-		}
-	}
-	return index
+    index := -1
+    s_array := []rune(s)
+    f_array := []rune(toFind)
+    if len(f_array) == 0 {
+        return 0
+    } else if len(s_array) < len(f_array) {
+        return -1
+    } else {
+        for i := 0; i < len(s_array); i++ {
+            if s_array[i] == f_array[0] {
+                index = i
+                if len(f_array) == 1 {
+                    return index
+                }
+                for j := 1; j < len(f_array) && j+i < len(s_array); j++ { // si j[1]
+                    // tant que j est < a la len find, et j + i < len(s_array) ?
+                    if s_array[i+j] == f_array[len(f_array)-1] { // dernière ligne
+                        return index
+                    }
+                    if s_array[i+j] != f_array[j] {
+                        return -1
+                    }
+                }
+            }
+        }
+    }
+    return index
 }
